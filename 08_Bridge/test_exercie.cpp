8d30526535fab0aa9c9f76de92b46c81dac4626f
arpad.balog <arpad.balogh@adaptiverecognition.tech>
2026-05-23 11:06:29 +0200

    Topic 8 - Bridge addition

--- 08_Bridge/test_exercie.cpp
+++ 08_Bridge/test_exercie.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 using namespace std;
 
+// the implementation side
+// this does the low-level work behind abstraction
+// so how to create the abstraction
 struct Renderer
 {
   virtual string what_to_render_as() const = 0;
@@ -17,12 +20,16 @@ struct RasterRenderer : Renderer
   string what_to_render_as() const override { return "pixels";} ;
 };
 
-
+// abstraction side
+// i want this, but it doesnt know how to draw it
+// so you create a Shape and tell it how to make it
 struct Shape
 {
 protected:
+    // this is the bridge part
     const Renderer& renderer;
     string name; // this has to be 2. bc init order :D
+    // lets every Shape use any Renderer
     Shape(const Renderer &renderer, const string& name) : renderer(renderer), name(name) {}
 public:
     string str() const { return "Drawing " + name + " as " + renderer.what_to_render_as(); }
